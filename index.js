6a55b72fcfd251070dfe7b6646680238bba5cb5c
mehediScriptDev <mehedi25122003@gmail.com>
2025-06-25 21:17:50 +0600

    backend started

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (res,req)=>{
+    req.send('job is fallen from the sky')
+})
