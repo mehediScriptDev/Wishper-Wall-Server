b56ccd2ab046dd31aef0ecc38057cc341f4258cc
mehediScriptDev <mehedi25122003@gmail.com>
2025-06-26 19:47:37 +0600

    got data from backend

--- index.js
+++ index.js
@@ -31,6 +31,15 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    const postCollections = client.db('wishperWall').collection('wishpers');
+
+    app.get('/posts', async(req,res)=>{
+      const cursor = postCollections.find();
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
